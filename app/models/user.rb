4985241d8b79888d183351a95d1771794b3e203c
Emily Welsch <emwelsch@gmail.com>
2018-08-08 13:32:49 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
-class User
-attr_accessor :username, :password, :balance
-
-def initialize(args={})
-  @username = args[:username]
-  @password = args[:password]
-  @balance = args[:balance]
-end 
+class User < ActiveRecord::Base
+# attr_accessor :username, :password, :balance
+# 
+# def initialize(args={})
+#   @username = args[:username]
+#   @password = args[:password]
+#   @balance = args[:balance]
+# end 
 
 end
