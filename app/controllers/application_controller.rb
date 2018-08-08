cf25948b59d494e40d65989c38675ccc4b8a5916
Emily Welsch <emwelsch@gmail.com>
2018-08-08 13:58:03 +0000

    automatically backed up by learn

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,31 @@
+require_relative '../../config/environment'
+class ApplicationController < Sinatra::Base
+  configure do
+    set :views, Proc.new { File.join(root, "../views/") }
+    enable :sessions unless test?
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/users/home'
+    end
+    redirect '/sessions/login'  
+  end
+
+  get '/account' do
+
+  end
+
+  get '/logout' do
+
+  end
+
+
+end
