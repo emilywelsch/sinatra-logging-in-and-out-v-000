e85ff5340ac396f90c6ed1c67896d3a51bbaf036
Emily Welsch <emwelsch@gmail.com>
2018-08-08 13:18:25 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
+class User
+attr_accessor :username, :password, :balance
 
+def initialize(args={})
+  @username = args[:username]
+  @password = args[:password]
+  @balance = args[:balance]
+end 
+
+end
 
